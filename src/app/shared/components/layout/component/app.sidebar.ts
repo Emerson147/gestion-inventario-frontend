55394127426e8c21240533a688b1acb75eb276c4
migatte <emersontec147@gmail.com>
2026-01-23 13:08:06 -0500

    feat: corrigiendo el la hamburguesa en modo movil

--- src/app/shared/components/layout/component/app.sidebar.ts
+++ src/app/shared/components/layout/component/app.sidebar.ts
@@ -2,13 +2,16 @@ import { Component, ElementRef, inject } from '@angular/core';
 import { AppMenu } from './app.menu';
 
 @Component({
-    selector: 'app-sidebar',
-    standalone: true,
-    imports: [AppMenu],
-    template: ` <div class="layout-sidebar bg-surface-0/95 backdrop-blur-md border-r border-surface-border shadow-lg transition-all duration-300">
-        <app-menu></app-menu>
-    </div>`
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [AppMenu],
+  template: ` <div
+    class="layout-sidebar bg-surface-0/95 backdrop-blur-md border-r border-surface-border"
+  >
+    <app-menu></app-menu>
+  </div>`,
+  styles: [':host { display: contents; }'],
 })
 export class AppSidebar {
-    el = inject(ElementRef);
+  el = inject(ElementRef);
 }
